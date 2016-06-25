6453c82ca1579d2eeab4af84d0adfbfbc34ce3a6
kingzhi <zyj10222@126.com>
2016-06-25 15:19:52 +0800

    init project

---
+++ src/components/Main.js
@@ -0,0 +1,43 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+//获取图片相关数据
+let imageDatas = require('../data/imagesDatas.json');
+
+// 利用自执行函数，将图片名信息转成图片URL信息
+imageDatas = (function genImageURL(imageDatasArr) {
+	for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+		var singleImageData = imageDatasArr[i];
+
+		singleImageData.imageURL = require('../images/' +
+			singleImageData.fileName);
+
+		imageDatasArr[i] = singleImageData;
+	}
+	return imageDatasArr;
+})(imageDatas);
+
+
+
+class AppComponent extends React.Component {
+	render() {
+		return (
+			<section className="stage">
+				<section className="img-sec">
+				</section>
+				
+				<nav>
+
+				</nav>
+
+			</section>
+		);
+	}
+}
+
+AppComponent.defaultProps = {};
+
+export
+default AppComponent;
